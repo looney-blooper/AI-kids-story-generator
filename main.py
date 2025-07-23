93ace134bb91aa6a09b50b229ae1e5e340a29f38
looney-blooper <mithunarulmani017@gmail.com>
2025-07-23 13:55:42 +0530

    added story to image

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from AI_model.prompt_templates import generate_image_prompt, generate_story_prom
 from AI_model.image_generator import generate_image
 from AI_model.generate_story import generate_story 
 from AI_model.extract_story_data import extract_story_data, extract_story_steps, extract_visual_descriptions
-import ast
+from post_image_generation.add_text_layer import add_text_to_image
 
 if __name__ == "__main__":
     # Step 1: Generate the story from question
-    question = "A detective duck wearing a detective costume, solving a murder mystery in a small town."
+    question = "A detective duck wearing a detective costume, solving a mystery of a 'egg' in a small town."
     raw_story = generate_story(question)
     print("Story generated successfully!")
     # Step 2: Extract structured information from the story
@@ -22,6 +22,6 @@ if __name__ == "__main__":
             visual_scene_description=story_steps[i]["visual_scene_description"],
         )
         print(i+1)
-        generated_img = generate_image(image_prompt,i+1)
-
+        generated_img_path = generate_image(image_prompt,i+1)
+        add_text_to_image(generated_img_path, story_steps[i]["story_point"])
 
